9d826e79bd1bde79d40c05bc3e3a5a3998b82363
Jose SR <josesanchez09190@gmail.com>
2024-03-26 02:01:34 -0600

    update basic list ui

--- app/src/main/java/com/jsrdev/basiclist/MainActivity.kt
+++ app/src/main/java/com/jsrdev/basiclist/MainActivity.kt
@@ -6,8 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.jsrdev.basiclist.ui.samples.MyApp
 import com.jsrdev.basiclist.ui.theme.BasicListTheme
-import com.jsrdev.basiclist.ui.tutorial.composables.Conversation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +16,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicListTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    Conversation()
-                    //MyApp(modifier = Modifier.fillMaxSize())
+                    //Conversation()
+                    MyApp(modifier = Modifier.fillMaxSize())
                 }
             }
         }
