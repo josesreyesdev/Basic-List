970fdc6b820982497c8306f4264e281be35ccc5c
Jose SR <josesanchez09190@gmail.com>
2024-03-26 01:47:40 -0600

    added basic example chat ui

---
+++ app/src/main/java/com/jsrdev/basiclist/ui/tutorial/composables/Composables.kt
@@ -0,0 +1,123 @@
+package com.jsrdev.basiclist.ui.tutorial.composables
+
+import android.content.res.Configuration
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jsrdev.basiclist.R
+import com.jsrdev.basiclist.ui.theme.BasicListTheme
+import com.jsrdev.basiclist.ui.tutorial.data.Message
+
+@Composable
+fun MessageCard(msg: Message) {
+
+    // Box => Apila los elementos.
+    // Row => Organiza los elementos horizontalmente.
+    // Column => Organiza los elementos verticalmente.
+
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.bad_bunny),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column (modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation() {
+    val messages = mutableListOf<Message>()
+    repeat(6) {
+        messages.addAll(
+            listOf(
+                Message(author = "ANDROID", body = "Jetpack Compose views"),
+                Message(author = "JSR", body = "Android Developer Jr"),
+                Message(author = "GAB", body = "She is my sister"),
+                Message(author = "IOS", body = "SwiftUI views"),
+                Message(author = "YEGOS", body = "I love motorcycles"),
+                Message(author = "Lorel", body = "YEGOS and GAB their mother"),
+            )
+        )
+    }
+    
+    LazyColumn(Modifier.padding(vertical = 2.dp)) {
+        items(items = messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    BasicListTheme {
+        Surface {
+            Conversation()
+        }
+    }
+}
---
+++ app/src/main/java/com/jsrdev/basiclist/ui/tutorial/data/Message.kt
@@ -0,0 +1,3 @@
+package com.jsrdev.basiclist.ui.tutorial.data
+
+data class Message(val author: String, val body: String)
